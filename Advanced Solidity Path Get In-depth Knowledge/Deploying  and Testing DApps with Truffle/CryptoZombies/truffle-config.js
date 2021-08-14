e98abbb2e74e0d751cc9d1b9ef6726179fcd9045
ARNAB DAS <raj713335@gmail.com>
2021-08-14 21:41:59 +0530

    Add files via upload

---
+++ Advanced Solidity Path Get In-depth Knowledge/Deploying  and Testing DApps with Truffle/CryptoZombies/truffle-config.js
@@ -0,0 +1,40 @@
+
+//1. Initialize `truffle-hdwallet-provider`
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+// Set your own mnemonic here
+const mnemonic = "YOUR_MNEMONIC";
+
+// Module exports to make this configuration available to Truffle itself
+module.exports = {
+  // Object with configuration for each network
+  networks: {
+    // Configuration for mainnet
+    mainnet: {
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/2915f63e64f44dc08d5377b3c485fe32")
+      },
+      network_id: "1"
+    },
+    // Configuration for rinkeby network
+    rinkeby: {
+      // Special function to setup the provider
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/2915f63e64f44dc08d5377b3c485fe32")
+      },
+      network_id:4 
+    },
+    loom_testnet: {
+  provider: function() {
+    const privateKey = 'YOUR_PRIVATE_KEY'
+    const chainId = 'extdev-plasma-us1';
+    const writeUrl = 'http://extdev-plasma-us1.dappchains.com:80/rpc';
+    const readUrl = 'http://extdev-plasma-us1.dappchains.com:80/query';
+    return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+    },
+  network_id: '9545242630824'
+}
+  }
+}; 
